0555d62d46bf793f0b3084e0ce885c22de8a9209
Edison Alexander Giraldo Pulgarin <edgiraldo@personalsoft.com.co>
2020-10-17 15:41:56 -0500

    se adiciona el listar de usuarios

--- src/app/models/User.model.ts
+++ src/app/models/User.model.ts
@@ -10,3 +10,36 @@ export interface IUser {
     fecha_Nacimiento: Date;
     id_rol: number;
 }
+
+export class User {
+    id_Usuario: number;
+    email: string;
+    password: string;
+    username: string;
+    nombre: string;
+    apellido: string;
+    direccion: string;
+    telefono: string;
+    fecha_Nacimiento: Date;
+    id_rol: number;
+
+    constructor(email: string,
+        password: string,
+        username: string,
+        nombre: string,
+        apellido: string,
+        direccion: string,
+        telefono: string,
+        fecha_Nacimiento: Date,
+        id_rol: number) {
+          this.email = email;
+          this.password = password;
+          this.username = username;
+          this.nombre = nombre;
+          this.apellido = apellido;
+          this.direccion = direccion;
+          this.telefono = telefono;
+          this.fecha_Nacimiento = fecha_Nacimiento;
+          this.id_rol = id_rol;
+    }
+}
--- src/app/services/Impl/users.service.ts
+++ src/app/services/Impl/users.service.ts
@@ -2,16 +2,27 @@ import { Injectable } from '@angular/core';
 import { ServiceService } from '../service.service';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IUser, User } from 'src/app/models/User.model';
+import { IRole } from 'src/app/models/Role.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService extends ServiceService<any>{
+export class UsersService extends ServiceService<any> {
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     super();
     this.apiUrl = environment.apiRest;
     this.httpClient = http;
-    this.resource = 'usuarios/';
+    this.resource = 'usuarios';
+  }
+
+  public getUsers(): Observable<IUser[]> {
+    const path = ``;
+    // let params = new HttpParams();
+    // params = params.append('idCase', String(idCase));
+    return this.executeGet(path).pipe(map((resp: IUser[]) => resp));
   }
 }
